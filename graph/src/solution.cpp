23cc2281e17a0ac8203568058e0002102b300c83
Aaron Santana Valdelomar <84732450+Santaval@users.noreply.github.com>
2024-11-14 16:51:32 -0600

    add cpp project structure

---
+++ graph/src/solution.cpp
@@ -0,0 +1,14 @@
+// Copyright <year> <You>
+#include <iostream>
+
+/**
+ * @brief Start program execution.
+ *
+ * @return Status code to the operating system, 0 means success.
+ */
+int main() {
+  // TODO(you): Analyze the problem and be sure you understand each piece of
+  // input and output. Solve the problem by creating an algorithm, and test it.
+  // Copy the algorithm within source code comments. Finally implement the
+  // algorithm's instructions in the C applying good programming practices.
+}
